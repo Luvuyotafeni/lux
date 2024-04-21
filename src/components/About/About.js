67b7cab6118cc986c15d418f2d63668db7b750c6
luvuyotafeni <luvuyotafeni012@gmail.com>
2024-04-21 20:58:48 +0200

    About page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,9 +4,29 @@ import './About.css'
 
 function About() {
   return (
-    <section id='about' className='about'>
-    
+    <section id='about' className='about_page' >
+      <div className='about'>
+      <p >ABOUT US</p>
+      </div>
+      <div className='outside-bg'>
+        <p>
+          Welcome to Lux Restaurant, where culinary excellence meets warm hospitality. Lux Restaurant is the brainchild of Luvuyo Tafeni,
+          a visionary entrepreneur with a passion for creating unforgettable dining experiences. Situated in the heart of Cape Town creativity
+          , Lux Restaurant has quickly become a beloved destination for food enthusiasts and connoisseurs alike.
+          At Lux Restaurant, we believe that dining is not just about food; it's about crafting moments of joy and celebration. 
+          Our mission is to delight your senses with a fusion of flavors, artfully presented dishes, and a welcoming ambiance that feels like home.
+          Our culinary team, led by Chef Luvuyo Tafeni, brings a wealth of experience and creativity to every dish. 
+          From innovative twists on traditional favorites to inspired creations that showcase the best of local and 
+          seasonal ingredients, each bite at Lux Restaurant tells a story of passion and dedication to culinary excellence.
+          Whether you're joining us for a romantic dinner, a family celebration, or a business lunch, 
+          Lux Restaurant offers a range of dining experiences to suit every occasion. Our elegant dining room provides a sophisticated setting, while our cozy bar area is perfect for relaxing with handcrafted cocktails and small bites.
+          At Lux Restaurant, hospitality is more than a service—it's a philosophy. Our friendly and knowledgeable staff are here to ensure that your dining experience is nothing short of exceptional, from the moment you walk through 
+          our doors until the last bite of dessert.
+          We invite you to indulge in the Lux experience and discover why we are more than just a restaurant—we are a destination for those who appreciate the finer things in life. Join us at Lux Restaurant, where every meal is a celebration of taste, style, and the art of living well.
+        </p>
+      </div>
     </section>
+    
   );
 }
 
