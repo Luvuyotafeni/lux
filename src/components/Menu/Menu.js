5aa0b5730756539185aa29f9bf5f5a2a5610b3bf
luvuyotafeni <luvuyotafeni012@gmail.com>
2024-04-26 08:20:48 +0200

    addition of menu items

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,25 +1,35 @@
-// src/components/Menu.js
-import React from 'react';
-import './Menu.css'
+  import React from 'react';
+  import './Menu.css';
+  import Card from './card'; // Ensure correct import path for Card component
+  import data from './Menu-Api'; // Ensure correct import path for data
 
-function Menu() {
-  return (
-    <section>
-     <div className='menu_d' id='menu'>
-      <div>
-      <h2>Menu</h2>
-      </div>
-      <div>
-      <p>Our delicious menu...</p>
-      </div>
-     </div>
-     <div>
-      <div>
-        
-      </div>
-     </div>
-    </section>
-  );
-}
+  function Menu() {
+    return (
+      <section>
+        <div className='menu_d' id='menu'>
+          <div>
+            <h2>Menu</h2>
+          </div>
+          <div>
+            <p>Our delicious menu...</p>
+          </div>
+        </div>
+        <div className='menu_container'>
+          {data.map((val, index) => {
+            return (
+              <div key={index} className='menu_item'>
+              <Card
+                price={val.price}
+                title={val.title}
+                desc={val.desc}
+                
+              />
+              </div>
+            );
+          })}
+        </div>
+      </section>
+    );
+  }
 
-export default Menu;
+  export default Menu;
--- src/components/Menu/card.js
+++ src/components/Menu/card.js
@@ -1,15 +1,13 @@
-import React from "react"
+import React from "react";
 
-const Card = (props) => {
-return(
-    <>
-       <div>
-        <h2>{props.title}</h2>
-        <p>{props.desc}</p>
-        <p>{props.price}</p>
-       </div>
-    </>
-)
-}
+const Card = ({ title, desc, price }) => {
+  return (
+    <div > {/* Add the menu_item class */}
+      <h2>{title}</h2>
+      <p>{desc}</p>
+      <p>{price}</p>
+    </div>
+  );
+};
 
-export default Card
+export default Card;
